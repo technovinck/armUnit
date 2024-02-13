d35e06cc5eb9ae2a91203cbd216cd4c5eb5a4769
deko <deko1980@gmail.com>
2024-02-13 23:39:21 +0100

    Cleaned for mqtt control

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,23 @@
 #include <spi.h>
-#include "webpage.h"
 #include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <config.h> //enthält Zugangsdaten für das Heimnetz
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
 
 #define PCA9685_ADDR 0x40 // Adresse des PCA9685-Treibers
 #define SERVO_MIN_PULSEWIDTH 650
 #define SERVO_MAX_PULSEWIDTH 2350
 #define SERVO_MAX_DEGREE 180
 
+#define DEVICE "armUnit" // hier den unit-name aendern
+
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(PCA9685_ADDR);
 
 bool servoExecuted = false;
 
-//Webserver Funktionen
-void handleRoot(AsyncWebServerRequest *request) {
-  request->send_P(200, "text/html", rootPage);
-}
-
-void handleNotFound(AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "File Not Found");
-}
-
-void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *webSocketClient, AwsEventType type, void *arg, uint8_t *data, size_t len) {                      
-  switch (type) {
-    case WS_EVT_CONNECT:
-      Serial.printf("WebSocket client #%u connected from %s\n", webSocketClient->id(), webSocketClient->remoteIP().toString().c_str());
-      break;
-    case WS_EVT_DISCONNECT:
-      Serial.printf("WebSocket client #%u disconnected\n", webSocketClient->id());
-      break;
-    case WS_EVT_DATA:
-      AwsFrameInfo *info;
-      info = (AwsFrameInfo*)arg;
-      if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-        String message = String((char *)data);
-        int servoAngle = message.toInt();
-        if (servoAngle >= 0 && servoAngle <= SERVO_MAX_DEGREE) {
-          pwm.setPWM(0, 0, map(servoAngle, 0, SERVO_MAX_DEGREE, SERVO_MIN_PULSEWIDTH, SERVO_MAX_PULSEWIDTH));
-          Serial.println("Servo position set: " + String(servoAngle));
-          // Senden Sie eine MQTT-Nachricht, wenn der Servobefehl empfangen und ausgeführt wurde
-          client.publish("armUnit/status", "Servo command received and executed");
-          servoExecuted = true;
-        } else {
-          Serial.println("Invalid servo angle: " + String(servoAngle));
-        }
-      }
-      break;
-    case WS_EVT_PONG:
-    case WS_EVT_ERROR:
-      break;
-    default:
-      break;  
-  }
-}
-
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 // Funktion zum Empfangen von MQTT-Nachrichten
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -78,8 +31,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   message[length] = '\0'; // Nullterminierung hinzufügen
   
   // Nachricht als String ausgeben
-  Serial.println(message);
-  client.publish("armUnit/status", "Nachricht erhalten!");
+  client.publish(DEVICE"/status", "Nachricht erhalten!");
 
   // Hier können Sie die Payload weiter verarbeiten
 }
@@ -89,12 +41,12 @@ void reconnect() {
   // Wiederherstellen der MQTT-Verbindung
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    if (client.connect("armUnit", mqttUser, mqttPassword)) {
+    if (client.connect(DEVICE, mqttUser, mqttPassword)) {
       Serial.println("connected");
       // Hier kannst du die Nachricht senden
-      client.publish("armUnit/status", "Connected");
+      client.publish(DEVICE"/status", "Connected");
       // Abonnieren von Nachrichten
-      client.subscribe("armUnit/control");
+      client.subscribe(DEVICE"/control");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -105,51 +57,34 @@ void reconnect() {
 }
 
 void setup() {
-    // Beginnen Sie mit der seriellen Kommunikation
-    Serial.begin(115200);
-    Serial.println("Booting...");
+  // Beginnen Sie mit der seriellen Kommunikation
+  Serial.begin(115200);
+  Serial.println("Booting...");
 
-    // WiFi-Modus auf Station (STA) setzen
-    WiFi.mode(WIFI_STA);
-    
-    // Verbindung zum vorhandenen WLAN-Netzwerk herstellen
-    WiFi.begin(ssid, password);
-    Serial.print("Verbindung zum WLAN herstellen");
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("Verbunden mit WLAN");
+  // WiFi-Modus auf Station (STA) setzen
+  WiFi.mode(WIFI_STA);
+  
+  // Verbindung zum vorhandenen WLAN-Netzwerk herstellen
+  WiFi.begin(ssid, password);
+  Serial.print("Verbindung zum WLAN herstellen");
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Verbunden mit WLAN");
 
-    // Überprüfen, ob eine Verbindung hergestellt wurde
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("Keine Verbindung zum WLAN hergestellt. Access Point starten.");
+  // Überprüfen, ob eine Verbindung hergestellt wurde
+  if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Keine Verbindung zum WLAN hergestellt. Access Point starten.");
 
-        // WiFi-Modus auf Access Point (AP) setzen
-        WiFi.mode(WIFI_AP);
-        
-        // Access Point konfigurieren
-        WiFi.softAP(hotspotSSID, hotspotPassword);
-        Serial.println("Access Point gestartet.");
-    }
-
- 
-  // Initialisiere Servo PWM
-  pwm.begin();
-  pwm.setPWMFreq(60);  // Setze PWM-Frequenz auf 60 Hz
-
-  // Setup code...
-
-  // initialisiere Webserver
-  server.on("/", HTTP_GET, handleRoot);
-  server.onNotFound(handleNotFound);
-
-  ws.onEvent(onWebSocketEvent);
-  server.addHandler(&ws);
-
-  server.begin();
-  Serial.println("HTTP server started");
+      // WiFi-Modus auf Access Point (AP) setzen
+      WiFi.mode(WIFI_AP);
+      
+      // Access Point konfigurieren
+      WiFi.softAP(hotspotSSID, hotspotPassword);
+      Serial.println("Access Point gestartet.");
+  }
 
   // Initialisiere ArduinoOTA
   ArduinoOTA.onStart([]() {
@@ -179,21 +114,15 @@ void setup() {
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
 
-  // initialisiere WiFi-Verbindung
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  // Initialisiere Servo PWM
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Setze PWM-Frequenz auf 60 Hz
+
 }
 
 void loop() 
 {
   ArduinoOTA.handle();
-  ws.cleanupClients();
 
   // Überprüfen, ob eine Verbindung zum MQTT-Broker hergestellt ist
   if (!client.connected()) {
@@ -204,7 +133,7 @@ void loop()
 
   // Senden Sie eine MQTT-Nachricht, wenn der Servobefehl ausgeführt wurde
   if (servoExecuted) {
-    client.publish("armUnit/status", "Servo command executed");
+    client.publish(DEVICE"/status", "Servo command executed");
     servoExecuted = false;
   }
 }
